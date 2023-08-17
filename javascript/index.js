56ee2f2f9d6237e146e53358354bc870c2d5d5b8
allysearles <alyssa.searles001@mymdc.net>
2023-08-17 16:43:24 -0400

    Added Current Location to Select menu, with functionality.

--- javascript/index.js
+++ javascript/index.js
@@ -40,7 +40,10 @@ function updateTime() {
 }
 function updateCity(event) {
 	let cityTimeZone = event.target.value;
-	let cityName = cityTimeZone.replace("_", "").split("/")[1];
+	if (cityTimeZone === "current") {
+		cityTimeZone = moment.tz.guess();
+	}
+	let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 	let cityTime = moment().tz(cityTimeZone);
 	let citiesElement = document.querySelector("#cities");
 	citiesElement.innerHTML = `
@@ -49,7 +52,7 @@ function updateCity(event) {
 			<h2>${cityName}</h2>
 			<div class="date">${cityTime.format("MMMM Do YYYY")}</div>
 		</div>
-		<div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+		<div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
 		"A"
 	)}</small></div>
 	</div>`;
